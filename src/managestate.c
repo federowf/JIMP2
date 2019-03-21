7b7554e67e7ee49bff7fef787f133dd96e9d7bb2
ozzyfilip <ozzyfilip@gmail.com>
2019-03-21 12:15:00 +0100

    update on state managment

--- src/managestate.c
+++ src/managestate.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "managestate.h"
+#include "upload.h"
 
-
-char** managestate(char** table,int enditeration, int length, int width){
+void managestate(game_struct* temp){
     int i;
     int j;
     int countneightbours = 0;
-    int iteration_now = 0;
 
-    int** lifetable = (int**)malloc(length * sizeof(int*));
-    for(i = 0; i++; i < length){
-        lifetable[i]=(int*)malloc(width * sizeof(int));
+    int** lifetable = (int**)malloc(temp->length * sizeof(int*));
+    for(i = 0; i++; i < temp->length){
+        lifetable[i]=(int*)malloc(temp->width * sizeof(int));
     }
 
-    while (iteration_now < enditeration) {
+    while (temp->iteration < temp->max_iteration) {
 
-        for (i = 0; i < width; i++) {
+        for (i = 0; i < temp->width; i++) {
 
-            for (j = 0; j < length; j++) {
+            for (j = 0; j < temp->length; j++) {
 
-                if (table[i - 1][j - 1] == '0' && i > 0 && j > 0) countneightbours++;
-                if (table[i][j - 1] == '0' && j > 0) countneightbours++;
-                if (table[i + 1][j - 1] == '0' && j > 0 && i < width - 1) countneightbours++;
+                if (temp->table[i - 1][j - 1] == '0' && i > 0 && j > 0) countneightbours++;
+                if (temp->table[i][j - 1] == '0' && j > 0) countneightbours++;
+                if (temp->table[i + 1][j - 1] == '0' && j > 0 && i < temp->width - 1) countneightbours++;
 
-                if (table[i - 1][j] == '0' && i > 0 && j > 0) countneightbours++;
-                if (table[i + 1][j] == '0' && j > 0 && i < width - 1) countneightbours++;
+                if (temp->table[i - 1][j] == '0' && i > 0 && j > 0) countneightbours++;
+                if (temp->table[i + 1][j] == '0' && j > 0 && i < temp->width - 1) countneightbours++;
 
-                if (table[i - 1][j + 1] == '0' && i > 0 && j < length - 1) countneightbours++;
-                if (table[i][j + 1] == '0' && j < length - 1) countneightbours++;
-                if (table[i + 1][j + 1] == '0' && j < length - 1 && i < width - 1) countneightbours++;
+                if (temp->table[i - 1][j + 1] == '0' && i > 0 && j < temp->length - 1) countneightbours++;
+                if (temp->table[i][j + 1] == '0' && j < temp->length - 1) countneightbours++;
+                if (temp->table[i + 1][j + 1] == '0' && j < temp->length - 1 && i < temp->width - 1) countneightbours++;
 
                 lifetable[i][j] = countneightbours;
                 countneightbours = 0;
@@ -37,20 +36,19 @@ char** managestate(char** table,int enditeration, int length, int width){
 
             }
         }
-        for (i = 0; i < width; i++) {
+        for (i = 0; i < temp->width; i++) {
 
-            for (j = 0; j < length; j++) {
+            for (j = 0; j < temp->length; j++) {
 
                 if(lifetable[i][j]==3){
-                    table[i][j] = '0';
+                    temp->table[i][j] = '0';
                 }
                 else if(lifetable[i][j] != 2){
-                    table[i][j] = '#';
+                    temp->table[i][j] = '#';
                 }
 
             }
         }
-        iteration_now++;
+        temp->iteration++;
     }
-    return table;
 }
