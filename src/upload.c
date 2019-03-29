29d4cf18936495a994c050080b2b4fa1aef07dbe
czarkom <mczarkowski@onet.pl>
2019-03-29 08:38:24 +0100

    modifying upload file

--- src/upload.c
+++ src/upload.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include "upload.h"
 
-void unpack_the_data(FILE* in, FILE* in2){
+game_struct* unpack_the_data(FILE* in, FILE* in2 ){
 
+    game_struct *result;
 
+    result= malloc(sizeof(game_struct);
+    in=fopen(in, "r");
+    in2=fopen(in2,"r");
+    fscanf(in,"%d%d%d", &(result->length), &(result->width)); //dimensions
+    fscanf(in2,"%d%d%d", &(result->length), &(result->width)); //musimy okreslic, ktore parametry maja byc zczytane
+
+    fclose(in);
+    fclose(in2);
+    return result;
 }
