e93aaffff1a6dce66baebe0aa8268285669cd2f7
czarkom <mczarkowski@onet.pl>
2019-03-30 14:52:13 +0100

    xyz

--- src/upload.c
+++ src/upload.c
@@ -7,9 +7,9 @@ game_struct* unpack_the_data(FILE* in, FILE* in2 ){
     game_struct *result;
 
     result= malloc(sizeof(game_struct);
-    in=fopen(in, "r");
-    in2=fopen(in2,"r");
-    fscanf(in,"%d%d%d", &(result->length), &(result->width)); //dimensions
+    in = fopen(in, "r");
+    in2 = fopen(in2,"r");
+    fscanf(in,"%d%d%d", &(result->length), &(result->width), &(max)); //dimensions
     fscanf(in2,"%d%d%d", &(result->length), &(result->width)); //musimy okreslic, ktore parametry maja byc zczytane
 
     fclose(in);
