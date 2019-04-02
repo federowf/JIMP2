7ee250435b29f5263aa1594d1a4fce99ba07fd00
ozzyfilip <ozzyfilip@gmail.com>
2019-04-02 20:03:32 +0200

    jednak cofamy

--- src/true_png.c
+++ src/true_png.c
@@ -7,6 +7,16 @@
 
 #include <png.h>
 
+int x, y;
+
+int width, height;
+png_byte color_type;
+png_byte bit_depth;
+
+png_structp png_ptr;
+png_infop info_ptr;
+int number_of_passes;
+png_bytep * row_pointers;
 
 void write_png_file(char* file_name) {
     FILE *fp = fopen(file_name, "wb");
