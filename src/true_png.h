c8d76b12413e8a8412c38e4cce3ed97d4443dab7
ozzyfilip <ozzyfilip@gmail.com>
2019-04-02 19:55:48 +0200

    jak bozia da to zadziala

---
+++ src/true_png.h
@@ -0,0 +1,10 @@
+#ifndef GAME_OF_LIFE_TRUE_PNG_H
+#define GAME_OF_LIFE_TRUE_PNG_H
+
+#include "upload.h"
+
+void write_png_file(char* file_name);
+
+void process_file(game_struct* main_game_data);
+
+#endif
