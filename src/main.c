518b5e8ca0fc9b1fe65bce25af018bf5fcd2bd27
ozzyfilip <ozzyfilip@gmail.com>
2019-04-02 20:02:19 +0200

    deadman walking to the deadline

--- src/main.c
+++ src/main.c
@@ -11,6 +11,17 @@
 #include <png.h>
 #include "true_png.h"
 
+int x, y;
+
+int width, height;
+png_byte color_type;
+png_byte bit_depth;
+
+png_structp png_ptr;
+png_infop info_ptr;
+int number_of_passes;
+png_bytep * row_pointers;
+
 int main(int argc, char** argv){
 
     if(argc < 2){
